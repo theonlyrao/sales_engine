32826f871b8d404f0ec1c1bc4ce38da3ab70d6e6
Ashwin Rao <theonlyrao@gmail.com>
2016-06-02 00:49:04 -0600

    Clean up some tests

--- app/controllers/api/v1/invoice_items/finder_controller.rb
+++ app/controllers/api/v1/invoice_items/finder_controller.rb
@@ -2,19 +2,39 @@ class Api::V1::InvoiceItems::FinderController < ApplicationController
   respond_to :json
   
   def show
-    query = { params.first[0] => params.first[1] }
-    if (params.first[0] == "created_at") || (params.first[0] == "updated_at")
-      query = { params.first[0] => Time.zone.parse(params.first[1]) }
-      respond_with InvoiceItem.find_by(query)
+    if params.first.first == "id"
+      respond_with InvoiceItem.find_by(id: params.first.last)
+    elsif params.first.first == "invoice_id"
+      respond_with InvoiceItem.find_by(invoice_id: params.first.last)
+    elsif params.first.first == "item_id"
+      respond_with InvoiceItem.find_by(item_id: params.first.last)
+    elsif params.first.first == "quantity"
+      respond_with InvoiceItem.find_by(quantity: params.first.last)
+    elsif params.first.first == "unit_price"
+      respond_with InvoiceItem.find_by(unit_price: params.first.last)
+    elsif params.first.first == "created_at"
+      respond_with InvoiceItem.find_by(created_at: params.first.last)
     else
-      respond_with InvoiceItem.find_by(query)
+      respond_with InvoiceItem.find_by(updated_at: params.first.last)
     end
   end
 
   def index
-    search_type = params.first.first
-    query = params.first.last
-    respond_with InvoiceItem.where("#{search_type} = ?", "#{query}")
+    if params.first.first == "id"
+      respond_with InvoiceItem.where("id = ?", "#{params.first.last}")
+    elsif params.first.first == "invoice_id"
+      respond_with InvoiceItem.where("invoice_id = ?", "#{params.first.last}")
+     elsif params.first.first == "item_id"
+      respond_with InvoiceItem.where("item_id = ?", "#{params.first.last}")
+    elsif params.first.first == "quantity"
+      respond_with InvoiceItem.where("quantity = ?", "#{params.first.last}")
+    elsif params.first.first == "unit_price"
+      respond_with InvoiceItem.where("unit_price = ?", "#{params.first.last}")
+    elsif params.first.first == "created_at"
+      respond_with InvoiceItem.where("created_at = ?", "#{params.first.last}")
+    else
+      respond_with InvoiceItem.where("updated_at = ?", "#{params.first.last}")
+    end
   end
   
 end
--- spec/requests/invoice_items_controller_spec.rb
+++ spec/requests/invoice_items_controller_spec.rb
@@ -1,25 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "InvoiceItemsControllers", type: :request do
+  before(:all) do
+    5.times do
+      create(:invoice_item)
+    end
+  end
   it "returns all invoice_items" do
     get "/api/v1/invoice_items.json"
     expect(response).to have_http_status(200)
     parsed_response = JSON.parse(response.body)
     expect(response.content_type).to eq("application/json")
-    expect(parsed_response.count).to eq(33)
-    expect(response.body).to include("item_id")
-    expect(response.body).to include("invoice_id")    
+    expect(parsed_response.count).to eq(5)
   end
   
   it "returns individual invoice_item" do
-    get "/api/v1/invoice_items/2.json"
+    invoice_item = InvoiceItem.last
+    get "/api/v1/invoice_items/#{invoice_item.id}.json"
     expect(response.content_type).to eq("application/json")
-    expect(response.body).to include("\"id\":2")
+    expect(response.body).to include("#{invoice_item.unit_price}")
   end
 
   it "can find a single invoice_item" do
-    invoice_item = InvoiceItem.find(2)
-    get "/api/v1/invoice_items/find?id=2"
+    invoice_item = InvoiceItem.last
+    get "/api/v1/invoice_items/find?id=#{invoice_item.id}"
     expect(response.content_type).to eq("application/json")
     expect(response.body).to include(invoice_item.quantity.to_s)
 
@@ -29,16 +33,18 @@ RSpec.describe "InvoiceItemsControllers", type: :request do
     time = "2016-05-31 16:57:31 UTC"
     invoice_item = InvoiceItem.create!(updated_at: time, unit_price: "23423")
     get "/api/v1/invoice_items/find?updated_at=2016-05-31T16:57:31.000Z"
-    expect(response.body).to include("234")
+    expect(response.body).to include("234.23")
 
     get "/api/v1/invoice_items/find?unit_price=234.23"
-    expect(response.body).to include("2016-05-31T16:57:31.000Z")
+    expect(response.body).to include("234.23")
   end
 
   it "can find multiple invoice_items" do
-    get "/api/v1/invoice_items/find_all?quantity=1"
+    create(:invoice_item, quantity: 3203)
+    create(:invoice_item, quantity: 3203)
+    get "/api/v1/invoice_items/find_all?quantity=3203"
     parsed_response = JSON.parse(response.body)
-    expect(parsed_response.count).to eq(13)    
+    expect(parsed_response.count).to eq(2)    
     expect(response.content_type).to eq("application/json")
   end
   

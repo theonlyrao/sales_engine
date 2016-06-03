5e2b8358a5e45fd57aef73fc9c2d982304a6c045
Ashwin Rao <theonlyrao@gmail.com>
2016-06-02 22:10:38 -0600

    Let customer find his favorite merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,5 @@ class Merchant < ActiveRecord::Base
   has_many :items
   has_many :transactions, through: :invoices
   has_many :invoice_items, through: :invoices
+  has_many :customers, through: :invoices
 end

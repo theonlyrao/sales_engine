8a93d54d6e639741f78913d14e8231f6013043be
Ashwin Rao <theonlyrao@gmail.com>
2016-06-03 01:47:08 -0600

    Refactor invoice_items to model

--- app/controllers/api/v1/invoice_items/finder_controller.rb
+++ app/controllers/api/v1/invoice_items/finder_controller.rb
@@ -2,26 +2,16 @@ class Api::V1::InvoiceItems::FinderController < ApplicationController
   respond_to :json
 
   def show
-    if params[:unit_price]
-      price = params[:unit_price].to_f * 100
-      respond_with InvoiceItem.find_by(unit_price: price)
-    else
-      respond_with InvoiceItem.find_by(invoice_item_params)
-    end
+    respond_with InvoiceItem.find_one(invoice_item_params)
   end
 
   def index
-    if params[:unit_price]
-      price = params[:unit_price].to_f * 100
-      respond_with InvoiceItem.where("unit_price = ?", "#{price.to_i}")
-    else
-      respond_with InvoiceItem.where(invoice_item_params)
-    end
+    respond_with InvoiceItem.find_all(invoice_item_params)
   end
 
   private
 
   def invoice_item_params
-    params.permit(:id, :invoice_id, :item_id, :quantity, :created_at, :updated_at)
+    params.permit(:id, :invoice_id, :item_id, :quantity, :unit_price, :created_at, :updated_at)
   end
 end

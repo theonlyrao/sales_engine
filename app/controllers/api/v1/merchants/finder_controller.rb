c9d4948357938dad4fa12784135a564baf95f6d3
Ashwin Rao <theonlyrao@gmail.com>
2016-06-02 23:29:50 -0600

    Refactor finders for items and merchants

--- app/controllers/api/v1/merchants/finder_controller.rb
+++ app/controllers/api/v1/merchants/finder_controller.rb
@@ -1,28 +1,42 @@
 class Api::V1::Merchants::FinderController < ApplicationController
   respond_to :json
   
+  # def show
+  #   if params.first.first == "id"
+  #     respond_with Merchant.find_by(id: params.first.last)
+  #   elsif params.first.first == "name"
+  #     respond_with Merchant.find_by(name: params.first.last)
+  #   elsif params.first.first == "created_at"
+  #     respond_with Merchant.find_by(created_at: params.first.last)
+  #   else
+  #     respond_with Merchant.find_by(updated_at: params.first.last)
+  #   end
+  # end
+
+  # def index
+  #   if params.first.first == "id"
+  #     respond_with Merchant.where("id = ?", "#{params.first.last}")
+  #   elsif params.first.first == "name"
+  #     respond_with Merchant.where("name = ?", "#{params.first.last}")
+  #   elsif params.first.first == "created_at"
+  #     respond_with Merchant.where("created_at = ?", "#{params.first.last}")
+  #   else
+  #     respond_with Merchant.where("updated_at = ?", "#{params.first.last}")
+  #   end
+  # end
+
   def show
-    if params.first.first == "id"
-      respond_with Merchant.find_by(id: params.first.last)
-    elsif params.first.first == "name"
-      respond_with Merchant.find_by(name: params.first.last)
-    elsif params.first.first == "created_at"
-      respond_with Merchant.find_by(created_at: params.first.last)
-    else
-      respond_with Merchant.find_by(updated_at: params.first.last)
-    end
+    respond_with Merchant.find_by(merchant_params)    
   end
 
   def index
-    if params.first.first == "id"
-      respond_with Merchant.where("id = ?", "#{params.first.last}")
-    elsif params.first.first == "name"
-      respond_with Merchant.where("name = ?", "#{params.first.last}")
-    elsif params.first.first == "created_at"
-      respond_with Merchant.where("created_at = ?", "#{params.first.last}")
-    else
-      respond_with Merchant.where("updated_at = ?", "#{params.first.last}")
-    end
+    respond_with Merchant.where(merchant_params)
+  end
+
+  private
+
+  def merchant_params
+    params.permit(:id, :name, :created_at, :updated_at)
   end
   
 end

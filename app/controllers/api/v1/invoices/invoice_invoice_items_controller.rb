608142eb5997adc70fa31629b32035b8294984a2
Ashwin Rao <theonlyrao@gmail.com>
2016-06-03 03:26:40 -0600

    Remove unncessary files

--- app/controllers/api/v1/invoices/invoice_invoice_items_controller.rb
+++
@@ -1,7 +0,0 @@
-class Api::V1::Invoices::InvoiceInvoiceItemsController < ApplicationController
-  respond_to :json
-  
-  def index
-    respond_with Invoice.find(params[:id]).invoice_items
-  end
-end
--- app/controllers/api/v1/invoices/invoice_transactions_controller.rb
+++
@@ -1,7 +0,0 @@
-class Api::V1::Invoices::InvoiceTransactionsController < ApplicationController
-  respond_to :json
-  
-  def index
-    respond_with Invoice.find(params[:id]).transactions
-  end
-end
